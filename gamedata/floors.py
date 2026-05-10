580123c64f15f1ed88175bf50ebcc9c4c76ff5b8
Last Penguin <41567062+Az-LastPenguin@users.noreply.github.com>
2026-05-10 12:19:52 +0300

    Abno librarian requirements

--- gamedata/floors.py
+++ gamedata/floors.py
@@ -28,10 +28,10 @@ vanilla_floors: list[Floor] = [
         abno_stages=[
             FloorStage(id=201001, name="Scorched Girl", checks=3, chapter=1),
             FloorStage(id=201002, name="Happy Teddy Bear", checks=3, chapter=3),
-            FloorStage(id=201003, name="Fairy Festival", checks=3, chapter=4),
-            FloorStage(id=201004, name="Queen Bee", checks=3, chapter=5),
+            FloorStage(id=201003, name="Fairy Festival", checks=3, chapter=4, req_librarians=2),
+            FloorStage(id=201004, name="Queen Bee", checks=3, chapter=5, req_librarians=3),
         ],
-        realization_stage=FloorStage(id=201005, name="Malkuth Realization", checks=8, chapter=5),
+        realization_stage=FloorStage(id=201005, name="Malkuth Realization", checks=8, chapter=5, req_librarians=4),
     ),
 
     # Yesod
@@ -41,10 +41,10 @@ vanilla_floors: list[Floor] = [
         abno_stages=[
             FloorStage(id=202001, name="Forsaken Murderer", checks=3, chapter=2),
             FloorStage(id=202002, name="All-Around Helper", checks=3, chapter=3),
-            FloorStage(id=202003, name="Singing Machine", checks=3, chapter=4),
-            FloorStage(id=202004, name="The Funeral of the Dead Butterflies", checks=3, chapter=5),
+            FloorStage(id=202003, name="Singing Machine", checks=3, chapter=4, req_librarians=2),
+            FloorStage(id=202004, name="The Funeral of the Dead Butterflies", checks=3, chapter=5, req_librarians=3),
         ],
-        realization_stage=FloorStage(id=202005, name="Yesod Realization", checks=8, chapter=5),
+        realization_stage=FloorStage(id=202005, name="Yesod Realization", checks=8, chapter=5, req_librarians=4),
     ),
 
     # Hod
@@ -54,10 +54,10 @@ vanilla_floors: list[Floor] = [
         abno_stages=[
             FloorStage(id=203001, name="Today's Shy Look", checks=3, chapter=2),
             FloorStage(id=203002, name="The Red Shoes", checks=3, chapter=3),
-            FloorStage(id=203003, name="Spider Bud", checks=3, chapter=4),
-            FloorStage(id=203004, name="Laetitia", checks=3, chapter=5),
+            FloorStage(id=203003, name="Spider Bud", checks=3, chapter=4, req_librarians=2),
+            FloorStage(id=203004, name="Laetitia", checks=3, chapter=5, req_librarians=3),
         ],
-        realization_stage=FloorStage(id=203005, name="Hod Realization", checks=8, chapter=6),
+        realization_stage=FloorStage(id=203005, name="Hod Realization", checks=8, chapter=6, req_librarians=4),
     ),
 
     # Netzach
@@ -67,10 +67,10 @@ vanilla_floors: list[Floor] = [
         abno_stages=[
             FloorStage(id=204001, name="Fragment of the Universe", checks=3, chapter=3),
             FloorStage(id=204002, name="Child of the Galaxy", checks=3, chapter=3),
-            FloorStage(id=204003, name="Porccubus", checks=3, chapter=4),
-            FloorStage(id=204004, name="Alriune", checks=3, chapter=5),
+            FloorStage(id=204003, name="Porccubus", checks=3, chapter=4, req_librarians=2),
+            FloorStage(id=204004, name="Alriune", checks=3, chapter=5, req_librarians=3),
         ],
-        realization_stage=FloorStage(id=204005, name="Netzach Realization", checks=8, chapter=5),
+        realization_stage=FloorStage(id=204005, name="Netzach Realization", checks=8, chapter=5, req_librarians=4),
     ),
 
     # Tiphereth
@@ -78,12 +78,12 @@ vanilla_floors: list[Floor] = [
         id=5,
         seph="Tiphereth",
         abno_stages=[
-            FloorStage(id=205001, name="The Queen of Hatred", checks=3, chapter=4),
-            FloorStage(id=205002, name="The Knight of Despair", checks=3, chapter=4),
-            FloorStage(id=205003, name="The King of Greed", checks=3, chapter=5),
-            FloorStage(id=205004, name="The Servant of Wrath", checks=3, chapter=6),
+            FloorStage(id=205001, name="The Queen of Hatred", checks=3, chapter=4, req_librarians=3),
+            FloorStage(id=205002, name="The Knight of Despair", checks=3, chapter=4, req_librarians=3),
+            FloorStage(id=205003, name="The King of Greed", checks=3, chapter=5, req_librarians=3),
+            FloorStage(id=205004, name="The Servant of Wrath", checks=3, chapter=6, req_librarians=3),
         ],
-        realization_stage=FloorStage(id=205005, name="Tiphereth Realization", checks=8, req_librarians=4, chapter=6),
+        realization_stage=FloorStage(id=205005, name="Tiphereth Realization", checks=8, chapter=6, req_librarians=4),
     ),
 
     # Gebura
@@ -91,12 +91,12 @@ vanilla_floors: list[Floor] = [
         id=6,
         seph="Gebura",
         abno_stages=[
-            FloorStage(id=206001, name="Little Red Riding Hooded Mercenary", checks=3, chapter=5),
-            FloorStage(id=206002, name="Big and Will be Bad Wolf", checks=3, chapter=5),
-            FloorStage(id=206003, name="Mountain of Smiling Bodies", checks=3, chapter=6),
-            FloorStage(id=206004, name="Nosferatu", checks=3, chapter=6),
+            FloorStage(id=206001, name="Little Red Riding Hooded Mercenary", checks=3, chapter=5, req_librarians=2),
+            FloorStage(id=206002, name="Big and Will be Bad Wolf", checks=3, chapter=5, req_librarians=3),
+            FloorStage(id=206003, name="Mountain of Smiling Bodies", checks=3, chapter=6, req_librarians=3),
+            FloorStage(id=206004, name="Nosferatu", checks=3, chapter=6, req_librarians=4),
         ],
-        realization_stage=FloorStage(id=206005, name="Gebura Realization", checks=8, chapter=6),
+        realization_stage=FloorStage(id=206005, name="Gebura Realization", checks=8, chapter=6, req_librarians=5),
     ),
 
     # Chesed
@@ -104,12 +104,12 @@ vanilla_floors: list[Floor] = [
         id=7,
         seph="Chesed",
         abno_stages=[
-            FloorStage(id=207001, name="Scarecrow Searching for Wisdom", checks=3, chapter=5),
-            FloorStage(id=207002, name="Warm-hearted Woodsman", checks=3, chapter=5),
-            FloorStage(id=207003, name="The Road Home & Scaredy Cat", checks=3, req_librarians=4, chapter=6),
-            FloorStage(id=207004, name="Ozma", checks=3, chapter=6),
+            FloorStage(id=207001, name="Scarecrow Searching for Wisdom", checks=3, chapter=5, req_librarians=2),
+            FloorStage(id=207002, name="Warm-hearted Woodsman", checks=3, chapter=5, req_librarians=2),
+            FloorStage(id=207003, name="The Road Home & Scaredy Cat", checks=3, chapter=6, req_librarians=4),
+            FloorStage(id=207004, name="Ozma", checks=3, chapter=6, req_librarians=4),
         ],
-        realization_stage=FloorStage(id=207005, name="Chesed Realization", checks=8, req_librarians=5, chapter=6),
+        realization_stage=FloorStage(id=207005, name="Chesed Realization", checks=8, chapter=6, req_librarians=5),
     ),
 
     # Binah
@@ -117,11 +117,11 @@ vanilla_floors: list[Floor] = [
         id=8,
         seph="Binah",
         abno_stages=[
-            FloorStage(id=208001, name="Big Bird", checks=3, req_librarians=2, chapter=6),
-            FloorStage(id=208002, name="Punishing Bird", checks=3, chapter=6),
-            FloorStage(id=208003, name="Judgement Bird", checks=3, chapter=7),
+            FloorStage(id=208001, name="Big Bird", checks=3, req_librarians=2, chapter=6, req_librarians=3),
+            FloorStage(id=208002, name="Punishing Bird", checks=3, chapter=6, req_librarians=3),
+            FloorStage(id=208003, name="Judgement Bird", checks=3, chapter=7, req_librarians=4),
         ],
-        realization_stage=FloorStage(id=208004, name="Binah Realization", checks=11, chapter=7),
+        realization_stage=FloorStage(id=208004, name="Binah Realization", checks=11, chapter=7, req_librarians=5),
     ),
 
     # Hokma
@@ -129,11 +129,11 @@ vanilla_floors: list[Floor] = [
         id=9,
         seph="Hokma",
         abno_stages=[
-            FloorStage(id=209001, name="The Burrowing Heaven", checks=3, chapter=6),
-            FloorStage(id=209002, name="The Price of Silence", checks=3, req_librarians=2, chapter=6),
-            FloorStage(id=209003, name="Blue Star", checks=3, chapter=7),
+            FloorStage(id=209001, name="The Burrowing Heaven", checks=3, chapter=6, req_librarians=3),
+            FloorStage(id=209002, name="The Price of Silence", checks=3, chapter=6, req_librarians=4),
+            FloorStage(id=209003, name="Blue Star", checks=3, chapter=7, req_librarians=4),
         ],
-        realization_stage=FloorStage(id=209004, name="Hokma Realization", checks=11, chapter=7),
+        realization_stage=FloorStage(id=209004, name="Hokma Realization", checks=11, chapter=7, req_librarians=5),
     ),
 
     # Keter
@@ -143,10 +143,10 @@ vanilla_floors: list[Floor] = [
         abno_stages=[
             FloorStage(id=210001, name="Bloodbath", checks=3, chapter=1),
             FloorStage(id=210002, name="Heart of Aspiration", checks=3, chapter=3),
-            FloorStage(id=210003, name="Pinocchio", checks=3, chapter=5), # TODO: Add librarian req
-            FloorStage(id=210004, name="The Snow Queen", checks=3, chapter=6),
+            FloorStage(id=210003, name="Pinocchio", checks=3, chapter=5, req_librarians=2),
+            FloorStage(id=210004, name="The Snow Queen", checks=3, chapter=6, req_librarians=3),
         ],
-        realization_stage=FloorStage(id=210009, name="Keter Realization", checks=8, chapter=7),
+        realization_stage=FloorStage(id=210009, name="Keter Realization", checks=8, chapter=7, req_librarians=5),
     ),
 ]
 vanilla_floors_dict: dict[int, Floor] = {floor.id: floor for floor in vanilla_floors}
